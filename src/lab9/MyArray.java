f70646dede77fb1fe8d55a0af28919e4fa749dbb
cavazoshj <46500116+cavazoshj@users.noreply.github.com>
2022-04-19 19:04:51 -0400

    final

--- src/lab9/MyArray.java
+++ src/lab9/MyArray.java
@@ -1,37 +1,50 @@
+/* Name: Hunter Cavazos
+ * Date: 4/18/2022
+ * Class: INFOC-211
+ * Description: This program simulates a game designed to make the user
+ * come up with words within 1 Hamiltonian Distance of the given word.
+ * Generic array objects are instantiated within the driver class and
+ * passed to this class for comparison. */
 package lab9;
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.ArrayList;								// Import ArrayList library
+import java.util.Random;								// Import Random library
 
 public class MyArray<T> {
+    /* Contains the MyArray attributes for a static random variable as well
+     * as the store ArrayList object. Includes a constructor and methods
+     * to calculate Hamiltonian distance and random index generation from
+     * the given list of words. This class accepts type T generic parameters.*/
     
-    static Random rand = new Random();
+    static Random rand = new Random();							// Instantiate static Random attribute
 
-    ArrayList<T> store = new ArrayList<>();
+    ArrayList<T> store = new ArrayList<>();						// Instantiate store ArrayList
 
     public MyArray(T[] arrayCopy) {
+	// Constructor for MyArray objects, accepting type T arrays
 
-	for (int i = 0; i < arrayCopy.length; i++)
-	    store.add(arrayCopy[i]);
+	for (int i = 0; i < arrayCopy.length; i++)					// Traverse parameter array
+	    store.add(arrayCopy[i]);							// Append to store arraylist
     }
 
     public int distance(MyArray other) {
+	// Accepts object of MyArray type and calculates Hamiltonian distance
 
-	int distance = (this.store.size() - other.store.size());
-
-	for (int i = 0; i < this.store.size(); i++) {
-		if(!this.store.get(i).equals(other.store.get(i)))
-		    distance++;
+	int distance = Math.abs((this.store.size() - other.store.size()));		// Initialize distance with difference between
+											// both arrays
+	for (int i = 0; i < Math.min(this.store.size(), other.store.size()); i++) {	// Traverse the smaller array
+		if(!this.store.get(i).equals(other.store.get(i)))			// If elements are different
+		    distance++;								// Increment distance
 	}
 
-	return distance;
+	return distance;								// Return distance
     }
 
     public T sample() {
+	// Generates a random element from the store array
 	
-	int random = rand.nextInt(store.size());
+	int random = rand.nextInt(store.size());					// Initialize random value between 0 and array size
 
-	return store.get(random);
-
+	return store.get(random);							// Return random index
     }
 }
--- src/lab9/MyArrayTest.java
+++ src/lab9/MyArrayTest.java
@@ -1,60 +1,66 @@
+/* Name: Hunter Cavazos
+ * Date: 4/18/2022
+ * Class: INFOC-211
+ * Description: This program simulates a game designed to make the user
+ * come up with words within 1 Hamiltonian Distance of the given word.
+ * Generic array objects are instantiated within the driver class and
+ * passed to this class for comparison. */
 package lab9;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.ArrayList;								// Import ArrayList library
+import java.util.Scanner;								// Import Scanner library
 
 public class MyArrayTest {
+    /* Acts as the driver for the program. Contains string array of predetermined
+     * words for the program to choose from, instantiates a random String object
+     * from sample() method, converts selected word to MyArray char array, 
+     * */
 
     public static void main(String[] args) {
+	// Driver method for program
+	
+	System.out.println("Welcome to the Hamiltonian Distance Game!");		// Intro prompt
+	System.out.println("You will be given a word and must list as many"
+		+ " possible words that are 1 Hamiltonian distance from"
+		+ " the given word. Good luck!");
 
-	Scanner input = new Scanner(System.in);
+	Scanner input = new Scanner(System.in);						// Instantiate Scanner object
 
-	int counter = 0;
-	int dist = 1;
+	int counter = 0;								// Initialize counter
+	int dist = 1;									// Initialize dist
 
-	String[] words = new String[] {"banana", "pineapple", "pitbull", 
+	String[] words = new String[] {"banana", "pineapple", "pitbull", 		// Initialize words array with word list
 		"mousepad", "speakers", "chair", "baseball", "purple",
 		"television", "commercial"};
 
-	MyArray wordsArray = new MyArray(words);
+	MyArray wordsArray = new MyArray(words);					// Instantiate MyArray object from words array
 
-	String firstWord = (String) wordsArray.sample();
+	String firstWord = (String) wordsArray.sample();				// Instantiate String object from random index from sample()
 
-	Character[] firstArray = firstWord.chars().mapToObj(ch -> (char) ch)
+	Character[] firstArray = firstWord.chars().mapToObj(ch -> (char) ch)		// Instantiate Char array from firstWord
 		.toArray(Character[]::new);
 
-	MyArray first = new MyArray(firstArray);
+	MyArray first = new MyArray(firstArray);					// Instantiate first word object from firstArray Char array
 
-	String secondWord;
+	String secondWord;								// Instantiate secondWord String
 
-	System.out.println("Your word is: \t" + firstWord);
+	System.out.println("\nYour word is: \t" + firstWord);				// Output word selection
 
-	while (dist == 1) {
-	    System.out.print("Enter a word: ");
-	    secondWord = input.next();
+	while (dist == 1) {								// While dist != 1
+	    System.out.print("Enter a word: ");						// Prompt user for word input
+	    secondWord = input.next();							// Initialize secondWord from input
 
-	    Character[] secondArray = (secondWord).chars().mapToObj(ch -> (char) ch)
+	    Character[] secondArray = (secondWord).chars().mapToObj(ch -> (char) ch)	// Instantiate Char array from secondWord
 		    .toArray(Character[]::new);
 
-	    MyArray second = new MyArray(secondArray);
+	    MyArray second = new MyArray(secondArray);					// Instantiate second word object from secondWord Char array
 
-	    try {
-		dist = first.distance(second);
+	    dist = first.distance(second);						// Assign distance with return value from distance()
 
-		if (dist == 1)
-		    counter++;
-	    }
-
-	    catch (IndexOutOfBoundsException e) {
-		System.out.println("Words must be of equal length.");
-
-		System.out.print("Enter a word: ");
-		secondWord = input.next();
-	    }
+	    if (dist == 1)								// If dist = 1
+		counter++;								// Increment score counter
 	}
 
-	System.out.println("Results: " + counter);
-
+	System.out.println("Results: " + counter + " words.");				// Output results
     }
-
 }
